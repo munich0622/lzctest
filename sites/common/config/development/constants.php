e2781b8e310daf63832032f716e4e0802a6d20db
刘志超 <331579625@qq.com>
2016-07-28 14:49:09 +0800

    1

--- sites/common/config/development/constants.php
+++ sites/common/config/development/constants.php
@@ -12,19 +12,31 @@ define('PAY_TYPE_DOWN_UP', 2);
 //邀请注册费用
 define('REGISTER_MONEY', 0.01);
 
+define('PLATE_ONE_GRADE_ONE', 0.01);
+define('PLATE_ONE_GRADE_TWO', 0.02);
+define('PLATE_ONE_GRADE_THREE', 0.03);
 
-define('PLATE_ONE_GRADE_ONE', 300);
-define('PLATE_ONE_GRADE_TWO', 500);
-define('PLATE_ONE_GRADE_THREE', 2000);
 
-
-define('PLATE_TWO_GRADE_ONE', 4000);
-define('PLATE_TWO_GRADE_TWO', 6000);
-define('PLATE_TWO_GRADE_THREE', 20000);
+define('PLATE_TWO_GRADE_ONE', 0.04);
+define('PLATE_TWO_GRADE_TWO', 0.05);
+define('PLATE_TWO_GRADE_THREE', 0.06);
 
 define('PLATE_THREE_GRADE_ONE', 20000);
 define('PLATE_THREE_GRADE_TWO', 30000);
 define('PLATE_THREE_GRADE_THREE', 50000);
+
+// define('PLATE_ONE_GRADE_ONE', 300);
+// define('PLATE_ONE_GRADE_TWO', 500);
+// define('PLATE_ONE_GRADE_THREE', 2000);
+
+
+// define('PLATE_TWO_GRADE_ONE', 4000);
+// define('PLATE_TWO_GRADE_TWO', 6000);
+// define('PLATE_TWO_GRADE_THREE', 20000);
+
+// define('PLATE_THREE_GRADE_ONE', 20000);
+// define('PLATE_THREE_GRADE_TWO', 30000);
+// define('PLATE_THREE_GRADE_THREE', 50000);
 
 define('WEIXIN_OPENID_KEY', 'wx_openid_key');
 
