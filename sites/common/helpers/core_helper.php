fac7b801d8268f1af14a2c6e785d4ae6b0670d86
刘志超 <331579625@qq.com>
2016-07-30 17:34:14 +0800

    1

--- sites/common/helpers/core_helper.php
+++ sites/common/helpers/core_helper.php
@@ -220,6 +220,23 @@ function create_order_sn($type){
 }
 
 /**
+ * 将xml转为array
+ * @param string $xml
+ * @throws WxPayException
+ */
+function XmlToArray($xml)
+{
+    if(!$xml){
+        throw new WxPayException("xml数据异常！");
+    }
+    //将XML转为array
+    //禁止引用外部xml实体
+    libxml_disable_entity_loader(true);
+    return json_decode(json_encode(simplexml_load_string($xml, 'SimpleXMLElement', LIBXML_NOCDATA)), true);
+
+}
+
+/**
  * 	作用：array转xml
  */
 function arrayToXml($arr) {
