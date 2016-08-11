d917c3dc70216397effe718033582b36fd8fd0f9
刘志超 <331579625@qq.com>
2016-08-11 09:44:51 +0800

    1

--- sites/user.tianfeng.com/controllers/login.php
+++ sites/user.tianfeng.com/controllers/login.php
@@ -28,18 +28,20 @@ class Login extends MY_Controller{
 		$password = trim(strval($this->input->post('admin_psd',TRUE))) ;
 		
 		$salt = $this->db->select('salt')->where(array('phone'=>$phone))->get('user')->row_array();
-		if($salt){
+		if(isset($salt) && !empty($salt)){
 		    $password = en_pass($password, $salt['salt']);
 			$user = $this->db->where(array('phone'=>$phone,'password'=>$password))->get('user')->row_array();
+		}else{
+		    go('该用户不存在','/login/index');
 		}
 
-		if ($user) {
+		if (isset($user) && !empty($user)) {
 			#成功，将用户信息保存至session
 			$_SESSION['user'] = $user;
 			redirect('index/index');
 		} else {
 			# error
-			go('登录失败','/login/index');
+			go('密码不正确','/login/index');
 		}
 	}
 	
