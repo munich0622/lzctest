609726ad82e78f0463bbd76042b20c1f9b22b513
刘志超 <liuzhichao@doplatform.com>
2016-08-13 13:05:40 +0800

    1

--- sites/user.tianfeng.com/controllers/Index.php
+++ sites/user.tianfeng.com/controllers/Index.php
@@ -4,7 +4,7 @@ class Index extends Admin_Controller{
 	{
 		parent::__construct();
 		if(!$this->user) redirect('login/index');
-		
+		$this->load->database();
 	}
 	public function index(){
 		
@@ -12,4 +12,17 @@ class Index extends Admin_Controller{
 		$this->load->view('index');
 	}
 	
+	public function test(){
+	    $this->load->model('user_model');
+	    $sql = " SELECT * FROM tf_pay WHERE receive_uid = 0 AND status = 1 AND dakuan_id = 0 AND type = 2";
+	    $list = $this->db->query($sql)->result_array();
+	    foreach($list as $key=>$val){
+	        if($val['content'] == '升2费用'){
+	            $receive_uid = $this->user_model->get_parents($val['pay_uid'],2);
+	        }else{
+	            $receive_uid = $this->user_model->get_parents($val['pay_uid'],3);
+	        }
+	        $this->db->where('id',$val['id'])->update('pay',array('receive_uid'=>$receive_uid));
+	    }
+	}
 }
